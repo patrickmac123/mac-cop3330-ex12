2aa1f2c79e2715057e0d461550c1cc7a4dfbf385
patpatdotmp4 <47404725+patpatdotmp4@users.noreply.github.com>
2021-09-09 21:56:30 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,34 @@
+package org.example;
+import java.util.Scanner;
+import java.math.*;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Exercise 12 Solution
+ *  Copyright 2021 Patrick Mac
+ */
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print( "Enter the principal: " );
+        String stringPrincipal = scan.nextLine();
+        System.out.print( "Enter the rate of interest: " );
+        String stringRate = scan.nextLine();
+        System.out.print( "Enter the number of years: " );
+        String stringYears = scan.nextLine();
+
+        double principal = Double.parseDouble(stringPrincipal);
+        double rate = Double.parseDouble(stringRate);
+        double years = Double.parseDouble(stringYears);
+
+        double interest = rate / 100;
+        double total = principal*(1+(interest*years));
+
+        BigDecimal formatedTotal = new BigDecimal(total).setScale(2, RoundingMode.HALF_UP);
+
+
+        System.out.println("After "+stringYears+" years at "+stringRate+"%, the investment will be worth $"+formatedTotal+".");
+    }
+}
